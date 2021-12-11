992dabc686bc933064d67fdbe553c10bce88de84
VladimirPolitov <92723145+VladimirPolitov@users.noreply.github.com>
2021-12-11 12:32:16 +0500

    add homepage

---
+++ src/p2-homeworks/h5/pages/Junior-Plus.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Junior_Plus () {
+    return (
+        <div> Junior</div>
+    )
+}
+
+export default Junior_Plus
---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Junior () {
+    return (
+        <div> Junior</div>
+    )
+}
+
+export default Junior
