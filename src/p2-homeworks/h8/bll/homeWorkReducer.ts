97089a851a8ff59617285e92bcce24ca42cc107c
VladimirPolitov <92723145+VladimirPolitov@users.noreply.github.com>
2022-01-02 10:19:48 +0500

    add 8HW

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,22 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+
+type ActionType = {type: 'sort', payload: 'up' | 'down'} | {type: 'chek', payload: number}
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            const newState = [...state].sort((a, b) => {
+                if (a.name>b.name) return 1
+                else if (a.name<b.name) return -1
+                else return 0
+            })
+            return action.payload === 'up' ? newState : newState.reverse()
         }
-        case 'check': {
-            // need to fix
-            return state
+        case 'chek': {
+            return state.filter(a=> a.age >= action.payload)
         }
+
         default: return state
     }
 }
